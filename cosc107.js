cc9ee3f6c3bf9670dbc62f6694e746b1f114da2c
亗Maro <120358938+Marodev-c@users.noreply.github.com>
2024-02-27 21:00:13 +0100

    Update cosc107.js

--- cosc107.js
+++ cosc107.js
@@ -1,3 +1,4 @@
+/*Declare variable to store months of a year*/
 const months = [
     'January',
     'February',
@@ -12,7 +13,7 @@ const months = [
     'November',
     'December',
   ];
-  
+  /*Declare variable to store days in a week*/
   const weekdays = [
     'Sunday',
     'Monday',
@@ -26,30 +27,32 @@ const months = [
   const giveaway = document.querySelector('.giveaway');
   const deadline = document.querySelector('.deadline');
   const items = document.querySelectorAll('.deadline-format h4');
-  
+
+  /*Declare variables storing current time such as Year,month,day,hour,minute e.t.c*/
   let tempDate = new Date();
   let tempYear = tempDate.getFullYear();
   let tempMonth = tempDate.getMonth();
   let tempDay = tempDate.getDate();
 
+  /*Declare variable called futuredate and time in the future where the timer ends and starts again*/
   const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 
-  
+  /*Extracts the components from futuredate*/
   const year = futureDate.getFullYear();
-  
   const hours = futureDate.getHours();
   const minutes = futureDate.getMinutes();
+  /*Retrieves months from months array*/
   let month = futureDate.getMonth();
   month = months[month];
-  
+  /*Rerieves Days from weekdays array*/
   const weekday = weekdays[futureDate.getDay()];
-
   const date = futureDate.getDate();
-  giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} at ${hours}:${minutes}am`;
+  /*Updates the content giveaway from the html with the calaculated future date*/
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} at ${hours}:${minutes}am`;
   
   const futureTime = futureDate.getTime();
   
-
+  /*Countdown logic*/
   function getCountdownTimer() {
     const today = new Date().getTime();
   
